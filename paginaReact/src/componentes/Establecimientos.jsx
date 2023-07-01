0a87dea9f8627e01d73cb2754debd15ffe804492
sigleto <sigleto@gmail.com>
2023-07-01 09:33:24 +0200

    segundo intento de enlacess

--- paginaReact/src/componentes/Establecimientos.jsx
+++ paginaReact/src/componentes/Establecimientos.jsx
@@ -13,8 +13,8 @@ const Establecimientos=()=>{
     <h2>Elige categoría:</h2>
     
     <ul className="lista">
-    <NavLink to="/bares" target="_blank">BARES</NavLink><br></br><br></br>
-    <NavLink to="/restaurantes" target="_blank">RESTAURANTES</NavLink>
+    <NavLink to="/bares" >BARES</NavLink><br></br><br></br>
+    <NavLink to="/restaurantes" >RESTAURANTES</NavLink>
         
     </ul>
     </div> 
