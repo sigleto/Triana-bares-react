a90da5edcb8d8830ebe138339d4ce4dc70c4e384
sigleto <sigleto@gmail.com>
2023-07-02 13:30:10 +0200

    añado archivo servidor a paginaReact

---
+++ paginaReact/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import router from "./routes/routes.js"
+import dotenv from 'dotenv'
+dotenv.config();
+const app=express();
+
+mongoose.connect(process.env.MONGO_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
+  .then(() => {
+    console.log("Conectado a la base de datos de MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error al conectar a la base de datos:", error);
+  });
+
+
+app.use(cors())
+
+app.use(express.json()) 
+app.use(router) 
+
+  
+//Implementa "text" para que express pueda interpretar el texto,json o formularios que se envia con "req" ose recibe con "res"
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.listen(process.env.PORT||3000,()=>console.log("escuchando en 3000"))
