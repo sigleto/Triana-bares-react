e61425c8c8da9c8c3a502a813e6828e1acd7709f
Juan <124933284+sigleto@users.noreply.github.com>
2023-07-11 09:20:35 +0200

    Septima

--- paginaReact/src/componentes/Establecimientos.jsx
+++ paginaReact/src/componentes/Establecimientos.jsx
@@ -3,8 +3,13 @@ import { NavLink,Link } from "react-router-dom";
 
 import '../estilos/establecimientos.css'
 const Establecimientos=()=>{
-const url_cena = `https://www.thefork.es/search?cc=16774-c1d&cityId=506461&date=2023-07-18&gclid=CjwKCAjw2K6lBhBXEiwA5RjtCR9RXrvwrakxJqAKcnJ28kd4etMkS5mKIGIx4WTPrKd1WRuf7J32gBoC6tkQAvD_BwE&hour=1290&partySize=2&restaurantTagId[]=1062&timezone=Europe%2FMadrid`;
-    const url_comida = `https://www.thefork.es/search?cc=16774-c1d&cityId=506461&date=2023-07-18&gclid=CjwKCAjw2K6lBhBXEiwA5RjtCR9RXrvwrakxJqAKcnJ28kd4etMkS5mKIGIx4WTPrKd1WRuf7J32gBoC6tkQAvD_BwE&hour=840&partySize=2&restaurantTagId[]=1062&timezone=Europe%2FMadrid`;
+const fecha=new Date();
+const mes=fecha.getMonth()+1;
+const dia=fecha.getDate();
+const año=fecha.getFullYear();
+    
+ const url_cena = `https://www.thefork.es/search?cc=16774-c1d&cityId=506461&date=${año}-0${mes}-${dia}&gclid=CjwKCAjw2K6lBhBXEiwA5RjtCR9RXrvwrakxJqAKcnJ28kd4etMkS5mKIGIx4WTPrKd1WRuf7J32gBoC6tkQAvD_BwE&hour=1290&partySize=2&restaurantTagId[]=1062&timezone=Europe%2FMadrid`;
+ const url_comida = `https://www.thefork.es/search?cc=16774-c1d&cityId=506461&date=${año}-0${mes}-${dia}&gclid=CjwKCAjw2K6lBhBXEiwA5RjtCR9RXrvwrakxJqAKcnJ28kd4etMkS5mKIGIx4WTPrKd1WRuf7J32gBoC6tkQAvD_BwE&hour=840&partySize=2&restaurantTagId[]=1062&timezone=Europe%2FMadrid`;
 
      return (
     <>    
