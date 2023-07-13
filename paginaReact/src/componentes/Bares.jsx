ca47975aec94a146fffab6af0e484906798e0c3e
Juan <124933284+sigleto@users.noreply.github.com>
2023-07-13 06:33:57 +0200

    Bares1

--- paginaReact/src/componentes/Bares.jsx
+++ paginaReact/src/componentes/Bares.jsx
@@ -71,7 +71,126 @@ const Bares=()=>{
     <div className="estab">  
     <div className="bar">LA COARTADA</div>
     <ul>
-        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d23934279-Reviews-La_Coartada-Seville_Province_of_Seville_Andalucia.html">Ubicación</a></li>
+        <li><a href="import React from "react";
+import '../estilos/bares.css'
+const Bares=()=>{
+
+    return(
+<div className="contenido1">
+    <div className="estab">  
+    <div className="bar">LA PARRALA</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/La+Parrala/@37.3824545,-6.0019085,15z/data=!4m5!3m4!1s0x0:0xd99fca132859003!8m2!3d37.3824545!4d-6.0019085">Ubicación</a></li>
+        <li><a href="https://www.thefork.es/restaurante/la-parrala-r731694#booking">Reserva una mesa</a></li>
+        <li><a href="https://www.aeat/wlpl/RHCS-JDIT/HorarioOVP" target="_blank">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">LAS GOLONDRINAS 2</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Las+Golondrinas+-+Pag%C3%A9s+del+Corro/@37.3840763,-6.0055876,15z/data=!4m5!3m4!1s0x0:0x9532523f76e64bd0!8m2!3d37.3840811!4d-6.0055642">Ubicación</a></li>
+        <li><a href="">Reserva una mesa</a></li>
+        <li><a href="https://www.aeat/wlpl/RHCS-JDIT/HorarioOVP" target="_blank">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">EL ALBOROQUE</div>
+    <ul>
+        <li><a href="https://github.com/sigleto/paginaweb.git">Ubicación</a></li>
+        <li><a href="https://www.google.com/maps/reserve/v/dine/c/nqeAimyFvos?source=pa&hl=es-ES&gei=epXqY8raCoSCkdUPoI6N2A8&sourceurl=http://www.google.es/service/MapsPlaceService/GetPlace?authuser%3D0%26gl%3Des%26hl%3Des%26q%3DAlboroque%2B,Tapas,%2BBar%2BRestaurantes">Reserva una mesa</a></li>
+        <li><a href="https://www.tripadvisor.co/ShowUserReviews-g187443-d1002508-r785585866-Restaurante_Alboroque-Seville_Province_of_Seville_Andalucia.html" target="_blank">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">LA BLANCA PALOMA</div>
+    <ul>
+        <li><a href="https://www.google.es/search?q=la%20blanca%20paloma%20sevilla&sxsrf=AJOqlzXoYPu8QbqaYQK6UoDBOAmcfpQBAg:1676318210985&source=hp&ei=_JXqY4KCNbSZkdUP3KaIwAM&iflsig=AK50M_UAAAAAY-qkDJ57JuRNCAjOE2PFLsldTG7jDtuD&oq=la+bla&gs_lcp=Cgdnd3Mtd2l6EAEYADIECCMQJzIECCMQJzIKCC4QsQMQgwEQQzIKCC4QrwEQxwEQQzIECAAQQzILCC4QgAQQxwEQrwEyCwguEIAEEMcBEK8BMgUIABCABDILCC4QgAQQxwEQrwEyBQgAEIAEOgoILhDHARCvARBDOgoILhCDARCxAxBDOg4ILhCABBCxAxDHARDRAzoRCC4QgAQQsQMQgwEQxwEQ0QM6CgguEMcBEK8BECc6CwgAEIAEELEDEIMBOgcILhDUAhBDOggIABCxAxCDAToOCC4QgAQQyQMQxwEQrwFQAFiqCmCXIGgAcAB4AIABpgGIAakGkgEDMC42mAEAoAEB&sclient=gws-wiz&tbs=lf:1,lf_ui:2&tbm=lcl&rflfq=1&num=10&rldimm=9096604268979346244&lqi=ChhsYSBibGFuY2EgcGFsb21hIHNldmlsbGFI77jO4-eAgIAIWjAQABABEAIYARgCGAMiGGxhIGJsYW5jYSBwYWxvbWEgc2V2aWxsYSoICAIQABABEAKSAQ1iYXJfYW5kX2dyaWxs&ved=2ahUKEwi_5vv7o5P9AhWk_7sIHZ35DbEQvS56BAgQEAE&sa=X&rlst=f#rlfi=hd:;si:9096604268979346244,l,ChhsYSBibGFuY2EgcGFsb21hIHNldmlsbGFI77jO4-eAgIAIWjAQABABEAIYARgCGAMiGGxhIGJsYW5jYSBwYWxvbWEgc2V2aWxsYSoICAIQABABEAKSAQ1iYXJfYW5kX2dyaWxs;mv:[[37.425395699999996,-5.9045476],[37.3807165,-6.011552399999999]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2">Ubicación</a></li>
+        <li><a href="">Reservar una mesa:Teléfono: 954 33 36 40</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d2360731-Reviews-Blanca_Paloma-Seville_Province_of_Seville_Andalucia.html" target="_blank">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">CASA DIEGO</div>
+    <ul>
+        <li><a href="https://www.google.es/search?tbs=lf:1,lf_ui:9&tbm=lcl&sxsrf=AJOqlzUr3rqUSowG0FfdC61wDVHzEVCIAA:1676318579953&q=casa+diego+sevilla&rflfq=1&num=10&rldimm=15263293002110069838&ved=2ahUKEwih5_OrpZP9AhWRHOwKHbLhAe4Qu9QIegQIDxAK#rlfi=hd:;si:15263293002110069838;mv:[[37.4061756,-5.9723272000000005],[37.3794257,-6.007726099999999]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e1!2m1!1e3!3sIAE,lf:1,lf_ui:9">Ubicación</a></li>
+        <li><a href="">No se reserva</a></li>
+        <li><a href="https://www.tripadvisor.co/Restaurant_Review-g187443-d6778587-Reviews-or50-Casa_Diego-Seville_Province_of_Seville_Andalucia.html" target="_blank">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">CASA RUPERTO</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Bar+Casa+Ruperto/@37.3819311,-6.0085324,15z/data=!4m6!3m5!1s0xd126c6963bc510d:0xc6d653b3474cfdfc!8m2!3d37.3819311!4d-6.0085324!16s%2Fg%2F1tznnlzk?entry=ttu">Ubicación</a></li>
+        <li><a href="">No se reserva</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d4040632-Reviews-Bar_Casa_Ruperto-Seville_Province_of_Seville_Andalucia.html" target="_blank">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">POR LA BOCA MUERE EL PEZ</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Restaurante+Por+la+boca+muere+el+pez+tapas/@37.3766714,-6.005816,15z/data=!4m2!3m1!1s0x0:0xbb965783ee86fad6?sa=X&ved=2ahUKEwiLlI6pppP9AhXQSKQEHbfFB5YQ_BJ6BQiHARAI">Ubicación</a></li>
+        <li><a href="">Reserva una mesa:Teléfono: 625 81 99 34</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d23375731-Reviews-Por_La_Boca_Muere_El_Pez_Tapas-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">VEGA 10</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Restaurante+LA+COARTADA/@37.3805722,-5.997898,15z/data=!4m6!3m5!1s0xd126db599605dcb:0x43ed23237335bd2a!8m2!3d37.3805722!4d-5.997898!16s%2Fg%2F11ptnc61wr?entry=ttu">Ubicación</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d1738841-Reviews-Vega_10-Seville_Province_of_Seville_Andalucia.html">Reserva una mesa</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d1738841-Reviews-Vega_10-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">LA COARTADA</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Restaurante+LA+COARTADA/@37.3805722,-5.997898,15z/data=!4m6!3m5!1s0xd126db599605dcb:0x43ed23237335bd2a!8m2!3d37.3805722!4d-5.997898!16s%2Fg%2F11ptnc61wr?entry=ttu">Ubicación</a></li>
+        <li><a href="https://www.thefork.es/restaurante/la-coartada-r721345#date=2023-07-13&hour=1290&partySize=2">Reserva una mesa</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d23934279-Reviews-La_Coartada-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">GASCONA</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Gascona/@37.3867464,-6.0048046,15z/data=!4m2!3m1!1s0x0:0x4ec5e366e3ccb3fa?sa=X&ved=2ahUKEwjrkPqV6IqAAxVCTqQEHYEkDuIQ_BJ6BAhPEAA&ved=2ahUKEwjrkPqV6IqAAxVCTqQEHYEkDuIQ_BJ6BAhjEAg">Ubicación</a></li>
+        <li><a href="https://www.thefork.es/restaurante/gascona-r726404#date=2023-07-13&hour=1230&partySize=2">Reserva una mesa</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d23788283-Reviews-Gascona-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">SOL Y SOMBRA</div>
+    <ul>
+        <li><a href="https://www.google.com/maps/reserve/v/dine/c/WfmIeBEOcrQ?source=pa&hl=es-ES&gei=1XmvZJrxI-btkdUPo760uAc&sourceurl=http://www.google.es/service/MapsPlaceService/GetPlace?authuser%3D0%26gl%3Des%26hl%3Des%26q%3DTaberna%2BSol%2By%2BSombra">Ubicación</a></li>
+        <li><a href="https://www.thefork.es/restaurante/sol-y-sombra-r17946#date=2023-07-13&hour=1260&partySize=2">Reserva una mesa</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d1153685-Reviews-Sol_y_Sombra-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">CASA REMESAL</div>
+    <ul>
+        <li><a href="https://www.google.es/maps/place/Bar+Casa+Remesal/@37.3837026,-6.0009529,15z/data=!4m2!3m1!1s0x0:0xd1d4aff642c0d6fe?sa=X&ved=2ahUKEwiF3Kyd6oqAAxWzVaQEHa01Af0Q_BJ6BAhOEAA&ved=2ahUKEwiF3Kyd6oqAAxWzVaQEHa01Af0Q_BJ6BAhgEAg">Ubicación</a></li>
+        <li><a href="">Reserva una mesa:Teléfono: 608 61 58 46</a></li>
+        <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d2341423-Reviews-Casa_Remesal-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+    <div className="estab">  
+    <div className="bar">EL ZERO 10</div>
+    <ul>
+        <li><a href="https://www.tripadvisor.es/LocationPhotoDirectLink-g187443-d8084486-i237736940-41cero10-Seville_Province_of_Seville_Andalucia.html">Ubicación</a></li>
+        <li><a href="https://elzero10.com/reservas/">Reserva una mesa</a></li>
+        <li><a href="https://www.tripadvisor.es/LocationPhotoDirectLink-g187443-d8084486-i237736940-41cero10-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
+    </ul>
+    </div>
+   
+   
+       
+    
+    </div>
+
+
+    )
+}
+export default Bares">Ubicación</a></li>
         <li><a href="https://www.thefork.es/restaurante/la-coartada-r721345#date=2023-07-13&hour=1290&partySize=2">Reserva una mesa</a></li>
         <li><a href="https://www.tripadvisor.es/Restaurant_Review-g187443-d23934279-Reviews-La_Coartada-Seville_Province_of_Seville_Andalucia.html">Estrellas de TripAdvisor</a></li>
     </ul>
