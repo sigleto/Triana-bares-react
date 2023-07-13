7824235a085352adfc5f0c07752a8c8f45365635
Juan <124933284+sigleto@users.noreply.github.com>
2023-07-13 10:25:28 +0200

    pijaditas

--- paginaReact/src/componentes/Home.jsx
+++ paginaReact/src/componentes/Home.jsx
@@ -13,8 +13,8 @@ const Home=()=>{
     <header className="container">
     <img className='logo'src="/LogoJuan.png" alt="juan" />
     <img className='imagen' src="principal.webp" alt="juan" />
-    <div className="parrafo"><p className="per">Esta página contiene una relación de establecimientos gastronómicos distribuidos en las categorías de "BARES" y "RESTAURANTES".La principal diferencia entre ambas es que en todos los sitios incluidos como restaurantes se puede reservar. Ello no quiere decir que también lo puedas hacer en algunos bares.
-        Así mismo, en los todos los establecimientos catalogados como bares, se pueden pedir tapas, lo que, tampoco implica que en algunos restaurantes también te las sirvan.
+    <div className="parrafo"><p className="per">Esta página contiene una relación de establecimientos gastronómicos distribuidos en las categorías de "BARES" y "RESTAURANTES".La principal diferencia entre ambas es que en todos los sitios incluidos como "restaurantes" se puede reservar. Ello no quiere decir que también lo puedas hacer en algunos bares.
+        Así mismo, en los todos los establecimientos catalogados como "bares", se pueden pedir tapas, lo que tampoco implica que en algunos restaurantes también te las sirvan.
         En la sección de <Link to="/Formulario">contactos</Link> me puedes dejar algún establecimiento que eches de menos en la relación y lo incluiré sin problema.</p>
      </div>
     </header>
