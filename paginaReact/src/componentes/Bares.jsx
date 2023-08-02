fc0ea8ad86c837ec71bc4392a7bf05df1ad17ebe
sigleto <sigleto@gmail.com>
2023-08-02 19:32:17 +0200

    cambio de fondo

--- paginaReact/src/componentes/Bares.jsx
+++ paginaReact/src/componentes/Bares.jsx
@@ -5,7 +5,7 @@ const Bares=()=>{
 const {bares}=useContext(Contexto)
     return(
        
-<div className="contenido1">
+<div className="contenido">
 {bares.map((item,index)=>(<div key={index} className="estab">  
      <div className="bar">{item.nombre}</div>
      <ul>
